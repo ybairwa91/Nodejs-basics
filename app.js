6419f547ebe1a41f0f0af5fc1262ca5d83ba1a72
Yogesh Kumar Bairwa <ybairwa786@gmail.com>
2023-09-22 14:56:45 +0530

    Nodejs basics

---
+++ app.js
@@ -0,0 +1,34 @@
+//--------CREATING OUR OWN SERVER-----------//
+
+/*
+
+//using http package to create own server
+const http = require("http"); //this is http package,helps to deal with request and response behaviour
+//return an object full of utilities method and properties provided by nodejs.
+
+function handleRequest(request, response) {
+  response.statusCode = 200;
+  response.end("<h1>Hello World!</h1>");
+}
+
+//now http is object and createServer is
+const server = http.createServer(handleRequest); //it will create a web server with the help of nodejs.
+//return an object
+
+server.listen(3000); //listen method provide requests function.
+
+//  amazon.com=>send a request to Amazon's server
+//  amazon.com:443
+
+//Handling requests
+
+
+*/
+
+//node js is just js so using evry feature of javascript is same as nodejs except some things like DOM which u particularly use to interact with the DOM.
+//remember that nodejs code execute or compute standalone on computer or desktop..
+
+//core syntax is always same.
+
+let userName = "Yogesh";
+console.log(userName);
